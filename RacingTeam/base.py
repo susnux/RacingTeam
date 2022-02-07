ad7417e22e2b6df2e7818256959f551f7302d8e6
Ferdinand Thiessen <rpm@fthiessen.de>
2022-02-07 16:02:03 +0100

    feat(helper): Helper for getting stop from callback data

--- RacingTeam/base.py
+++ RacingTeam/base.py
@@ -1,6 +1,7 @@
+import vvo
+
 from enum import Enum
 from typing import Iterable, Optional, Type, Union
-
 from telegram import Update
 
 
@@ -19,18 +20,32 @@ def get_data(update: Update) -> tuple[QueryTag, Optional[tuple]]:
     return update.callback_query.data
 
 
-def pattern_valid_tag(tag: QueryTag, data_types: list[Union[Type, Iterable[Type]]] = None):
+def get_stop_data(update: Update):
+    tag, data = get_data(update)
+    response = vvo.find_stops(data[0], shortcuts=True, limit=1)
+    if len(response.points) == 0:
+        raise RuntimeError(
+            "Called with invalid StopID or something went wrong, both should never happen!"
+        )
+    return tag, response.points[0], tuple(data[1:])
+
+
+def pattern_valid_tag(
+    tag: Union[QueryTag, Iterable[QueryTag]], data_types: list[Union[Type, Iterable[Type]]] = None
+):
     """Create a pattern / validator for callback data
 
     Validates the tag and optionally also the data types
     """
+    if isinstance(tag, QueryTag):
+        tag = [tag]
 
     def validator(data):
         valid = (
             isinstance(data, tuple)
             and len(data) == 2
             and isinstance(data[0], QueryTag)
-            and data[0] == tag
+            and data[0] in tag
         )
         if valid and data_types is not None:
             return len(data[1]) == len(data_types) and all(
